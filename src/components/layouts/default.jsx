f5874710cbd5321bc78783a3b8ea6e5e6dd3eb09
Juan Rivillas <jprivillaso@gmail.com>
2019-02-14 14:55:23 -0200

    Added background to styled component

--- src/components/layouts/default.jsx
+++ src/components/layouts/default.jsx
@@ -8,6 +8,9 @@ const Layout = styled.div`
   width: 100%;
   height: 100%;
   overflow: hidden;
+  background: url("../../../images/main_page.jpg") no-repeat center center;
+  background-size: cover;
+  background-position: center;
 `;
 
 const LayoutContent = styled.section`
@@ -19,11 +22,6 @@ const LayoutContent = styled.section`
 const layout = ({ children }) => (
   <Layout
     className="default_layout"
-    style={{
-      background: 'url("../../../images/main_page.jpg") no-repeat center center',
-      backgroundSize: 'cover',
-      backgroundPosition: 'center'
-    }}
   >
     <LayoutContent>
       <Header />
